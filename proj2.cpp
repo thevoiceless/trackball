f5b917b87b78f4b33301470ea033e033ebe66918
Riley Moses <victi.resurgo@gmail.com>
2012-09-27 19:35:39 -0600

    Clean up comments and formatting

--- proj2.cpp
+++ proj2.cpp
@@ -12,38 +12,29 @@
 
 using namespace std;
 
-/* --------------------------------------------- */
-/* ----- GLOBAL VARIABLES  --------------------- */
-/* --------------------------------------------- */
-
+// Global variables
 static const int VPD_DEFAULT = 800;
-
 static const int MENU_SLOWER = 1;
 static const int MENU_FASTER = 2;
 static const int MENU_STOP_RUN = 3;
-
 static const double TWOPI = (2.0 * M_PI);
 
-/* --------------------------------------------- */
-
-GLint wid;               /* GLUT window id; value asigned in main() and should stay constant */
-GLint vpw = VPD_DEFAULT; /* viewport dimensions; change when window is resized (resize callback) */
+// GLUT window id; value asigned in main() and should stay constant
+GLint wid;
+// Viewport dimensions; change when window is resized (via resize callback)
+GLint vpw = VPD_DEFAULT;
 GLint vph = VPD_DEFAULT;
 
-/* --------------------------------------------- */
-
-GLfloat angle1 = 0;  /* angles used in animation */
+// Angles used in animation
+GLfloat angle1 = 0;
 GLfloat angle2 = 0;
 GLfloat dangle1 = 0.0057;
 GLfloat dangle2 = 0.0071;
+// Whether or not to animate
+bool animate = true;
 
-bool animate = true;   /* animate or not? */
-
-/* --------------------------------------------- */
-/* ----- SET LIGHT SOURCE PROPERTIES ----------- */
-/* --------------------------------------------- */
-
-void init_lightsource ( )
+// Set light source properties
+void init_lightsource()
 {
   GLfloat light_ambient[] = { .0, .0, .0, 1.0 };
   GLfloat light_diffuse[] = { .9, .9, .9, 1.0 };
@@ -51,7 +42,7 @@ void init_lightsource ( )
   GLfloat light_position[] = { 200.0, 200.0, 200.0, 1.0 };
   GLfloat global_ambient[] = { 0.1, 0.1, 0.1, 1.0 };
 
-  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);   // 
+  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient); 
   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
   glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
@@ -59,21 +50,17 @@ void init_lightsource ( )
   glLightf(GL_LIGHT0,GL_LINEAR_ATTENUATION,0.0);
   glLightf(GL_LIGHT0,GL_QUADRATIC_ATTENUATION,0.0);
 
-  // ambient light intensity: global constant
-  // In OpenGL there is global ambient intensity (like Ia in the Phong model we discussed 
-  // in class.
-  // Additionally each light can add some amount to that ambient light.
+  // Ambient light intensity: global constant
+  // In OpenGL there is global ambient intensity, like Ia in the Phong model
+  // Additionally, each light can add some amount to that ambient light
   glLightModelfv(GL_LIGHT_MODEL_AMBIENT,global_ambient);
 
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
 }
 
-/* --------------------------------------------- */
-/* ------... AND MATERIAL PROPERTIES ----------- */
-/* --------------------------------------------- */
-
-void set_material_properties ( GLfloat r, GLfloat g, GLfloat b )
+// Set material properties
+void set_material_properties(GLfloat r, GLfloat g, GLfloat b)
 {
   GLfloat mat_specular[4] = { 0.0, 0.0, 0.0, 1.0 };
   GLfloat mat_ambient_and_diffuse[4] = { 0.5, 0.5, 0.5, 1.0 };
@@ -89,13 +76,10 @@ void set_material_properties ( GLfloat r, GLfloat g, GLfloat b )
  
 }
 
-/* --------------------------------------------- */
-/* ------ RENDERING ---------------------------- */
-/* --------------------------------------------- */
+// Rendering
 
-// a single cube extending from -1 to 1 in all dimensions
-
-GLuint draw_cube ( )
+// A single cube extending from -1 to 1 in all dimensions
+GLuint draw_cube()
 {
   glBegin(GL_QUADS);
 
@@ -144,11 +128,8 @@ GLuint draw_cube ( )
   glEnd();
 }
 
-/* --------------------------------------------- */
-
-// all cubes
-
-GLuint draw_scene ( )
+// All cubes
+GLuint draw_scene()
 {
   set_material_properties(1.0,1.0,1.0);
 
@@ -187,146 +168,137 @@ GLuint draw_scene ( )
   glPopMatrix();
 }
 
-/* --------------------------------------------- */
 
-/* draw callback: clear window, set up matrices, draw all cubes */
-
+// Draw callback: clear window, set up matrices, draw all cubes
 void draw()
 {
-  /* set the projection matrix */
+  // Set the projection matrix
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(8.0,1.0,15.0,25.0);
 
-  // ... and modelview matrix
+  // Set the modelview matrix
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
-  /* initialize light -- note that modelview matrix is identity here
-     so light source coordinates won't be transformed --
-     this is equivalent to specifying light source location in 
-     the world coordinates */
+  // Initialize light
+  // Note that modelview matrix is identity here so light source coordinates won't be transformed
+  // This is equivalent to specifying light source location in the world coordinates
   init_lightsource();
 
-  // update angles
+  // Update angles
   if (animate)
-    {
-      angle1 += dangle1;
-      angle2 += dangle2;
-    }
+  {
+    angle1 += dangle1;
+    angle2 += dangle2;
+  }
 
-  // build modelview matrix....
+  // Build modelview matrix
   glTranslatef(0,0,-20);
   glRotatef(angle1,1,2,3);
   glRotatef(angle2,-2,-1,0);
 
-  /* ensure we're drawing to the correct GLUT window */
+  // Ensure we're drawing to the correct GLUT window
   glutSetWindow(wid);
 
-  /* clear the color buffers */
+  // Clear the color buffers
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  /* draw cubes; note that flat shading is the default -- use glShadeModel to change */
+  // Draw cubes; note that flat shading is the default (use glShadeModel to change)
   draw_scene();
 
-  /* flush the pipeline */
+  // Flush the pipeline
   glFlush();
 
-  /* look at our handiwork */
+  // Look at our handiwork
   glutSwapBuffers();
 
-  // make sure everything is redrawn in the animation mode
+  // Make sure everything is redrawn in the animation mode
   if (animate)
-    glutPostRedisplay();  
+  {
+    glutPostRedisplay();
+  } 
 }
 
-/* --------------------------------------------- */
-/* ------- INTERACTION ------------------------- */
-/* --------------------------------------------- */
-
-/* handle mouse events */
-
+// Interaction
+// Handle mouse events
 void mouse_button(GLint btn, GLint state, GLint mx, GLint my)
 {
-  switch( btn ) {
+  switch(btn)
+  {
     case GLUT_LEFT_BUTTON:
       cout << "Left Button"; // remove this line from your final submission
-      switch( state ) {
+      switch(state)
+      {
         case GLUT_DOWN: 
-	  cout << " down" << endl; // remove this line from your final submission
-	  break;
-        case GLUT_UP:   
-	  cout << " up" << endl; // remove this line from your final submission
-	  break;
+          cout << " down" << endl; // remove this line from your final submission
+	        break;
+        case GLUT_UP:
+          cout << " up" << endl; // remove this line from your final submission
+          break;
       }
       break;
     case GLUT_MIDDLE_BUTTON:
       cout << "Middle Button"; // remove this line from your final submission
-      switch( state ) {
+      switch( state )
+      {
         case GLUT_DOWN: 
-	  cout << " down" << endl; // remove this line from your final submission
-	  break;
+	        cout << " down" << endl; // remove this line from your final submission
+	        break;
         case GLUT_UP:   
-	  cout << " up" << endl; // remove this line from your final submission
-	  break;
+	        cout << " up" << endl; // remove this line from your final submission
+	        break;
       }
       break;
     case GLUT_RIGHT_BUTTON:
       cout << "Right Button"; // remove this line from your final submission
-      switch( state ) {
+      switch( state )
+      {
         case GLUT_DOWN: 
-	  cout << " down" << endl; // remove this line from your final submission
-	  break;
+	        cout << " down" << endl; // remove this line from your final submission
+	        break;
         case GLUT_UP:   
-	  cout << " up" << endl; // remove this line from your final submission
-	  break;
+	        cout << " up" << endl; // remove this line from your final submission
+	        break;
       }
       break;
   }
 }
 
-/* --------------------------------------------- */
-
-// mouse moves with button down
-
+// Mouse moves with button down
 GLvoid button_motion(GLint mx, GLint my)
 {
   cout << "Motion with button down: " << mx << "," << my << endl; // remove this line from your final submission
   return;
 }
 
-/* --------------------------------------------- */
-
-// mouse moves with button up
-
+// Mouse moves with button up
 GLvoid passive_motion(GLint mx, GLint my)
 {
   cout << "Passive Motion: " << mx << "," << my << endl; // remove this line from your final submission
   return;
 }
 
-/* --------------------------------------------- */
 
-/* handle keyboard events; here, just exit if ESC is hit */
-
+// Handle keyboard events
 void keyboard(GLubyte key, GLint x, GLint y)
 {
-  switch(key) {
-    case 27:  /* ESC */
-              exit(0);
-
-    default:  break;
+  switch(key)
+  {
+    // Exit when escape is pressed
+    case 27:
+      exit(0);
+    default:
+      break;
   }
 }
 
-/* --------------------------------------------- */
 
-/* menu callback */
-
-void menu ( int value )
+// Menu callback
+void menu(int value)
 {
   switch(value)
-    {
+  {
     case MENU_SLOWER:
       dangle1 *= .5;
       dangle2 *= .5;
@@ -338,14 +310,11 @@ void menu ( int value )
     case MENU_STOP_RUN:
       animate = !animate;
       break;
-    }
+  }
 }
 
-/* --------------------------------------------- */
-
-/* handle resizing the glut window */
-
-GLvoid reshape ( GLint sizex, GLint sizey )
+// Handle resizing the window
+GLvoid reshape(GLint sizex, GLint sizey)
 {
   glutSetWindow(wid);
 
@@ -358,97 +327,88 @@ GLvoid reshape ( GLint sizex, GLint sizey )
   glutPostRedisplay();
 }
 
-/* --------------------------------------------- */
-/* -------- SET UP GLUT  ----------------------- */
-/* --------------------------------------------- */
-
+// Set up GLUT
 GLint init_glut(GLint *argc, char **argv)
 {
   GLint id;
 
   glutInit(argc,argv);
 
-  /* size and placement hints to the window system */
+  // Size and placement hints to the window system
   glutInitWindowSize(vpw, vph);
   glutInitWindowPosition(10,10);
 
-  /* double buffered, RGB color mode */
+  // Double-buffered, RGB color mode
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-  /* create a GLUT window (not drawn until glutMainLoop() is entered) */
+  // Create a GLUT window (not drawn until glutMainLoop() is entered)
   id = glutCreateWindow("MACS441 OpenGL Sample code");    
 
-  /* register callbacks */
-
-  /* window size changes */
+  // Register callbacks
+  // Window size changes
   glutReshapeFunc(reshape);
 
-  /* keypress handling when the current window has input focus */
+  // Keypress handling when the current window has input focus
   glutKeyboardFunc(keyboard);
 
-  /* mouse event handling */
-  glutMouseFunc(mouse_button);           /* button press/release        */
-  glutMotionFunc(button_motion);         /* mouse motion w/ button down */
-  glutPassiveMotionFunc(passive_motion); /* mouse motion with button up */
+  // Mouse event handling
+  // Button press/release
+  glutMouseFunc(mouse_button);
+  // Mouse motion with button down
+  glutMotionFunc(button_motion);
+  // Mouse motion with button up
+  glutPassiveMotionFunc(passive_motion);
 
-  /* window obscured/revealed event handler */
+  // Window obscured/revealed event handler
   glutVisibilityFunc(NULL);
 
-  /* handling of keyboard SHIFT, ALT, CTRL keys */
+  // Handling of keyboard SHIFT, ALT, CTRL keys
   glutSpecialFunc(NULL);
 
-  /* what to do when mouse cursor enters/exits the current window */
+  // What to do when mouse cursor enters/exits the current window
   glutEntryFunc(NULL);
 
-  /* what to do on each display loop iteration */
+  // What to do on each display loop iteration
   glutDisplayFunc(draw);
 
-  /* create menu */
+  // Create menu
   GLint menuID = glutCreateMenu(menu);
-  glutAddMenuEntry("slower",MENU_SLOWER);
-  glutAddMenuEntry("faster",MENU_FASTER);
-  glutAddMenuEntry("stop/run",MENU_STOP_RUN);
+  glutAddMenuEntry("slower", MENU_SLOWER);
+  glutAddMenuEntry("faster", MENU_FASTER);
+  glutAddMenuEntry("stop/run", MENU_STOP_RUN);
   glutSetMenu(menuID);
   glutAttachMenu(GLUT_RIGHT_BUTTON);
 
   return id;
 }
 
-/* --------------------------------------------- */
-/* ----- TYPICAL OPENGL SETTINGS FOR 3D -------- */
-/* --------------------------------------------- */
-
-void init_opengl( )
+// OpenGL settings for 3D
+void init_opengl()
 {
-  /* back-face culling on */
+  // Enable back-face culling
   glEnable(GL_CULL_FACE);
   glCullFace(GL_BACK);
 
-  /* automatically scale normals to unit length after transformation */
+  // Automatically scale normals to unit length after transformation
   glEnable(GL_NORMALIZE);
 
-  /* clear to BLACK */
+  // Clear to BLACK
   glClearColor(0.0, 0.0, 0.0, 1.0);
 
-  /* Enable depth test  */
+  // Enable depth test
   glEnable(GL_DEPTH_TEST);
 }
 
-/* --------------------------------------------- */
-/* --------------------------------------------- */
-/* --------------------------------------------- */
-
 GLint main(GLint argc, char **argv)
 {  
-  /* initialize GLUT: register callbacks, etc */
+  // Initialize GLUT: register callbacks, etc.
   wid = init_glut(&argc, argv);
 
-  /* any OpenGL state initialization we need to do */
+  // Take care of any OpenGL state initialization
   init_opengl();
 
+  // Start loop
   glutMainLoop();
 
   return 0;
 }
-
-/* --------------------------------------------- */
